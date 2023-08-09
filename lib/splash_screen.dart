b3377bddf3f6b9d093c0459d9101df092aea59b7
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-09 19:53:07 +0530

    commit

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:lottie/lottie.dart';
 import './insta_login_page.dart';
 
 void main() => runApp(const MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: SplashScreen(),
-));
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ));
 
 class SplashScreen extends StatefulWidget {
   // splash is initially immutable
@@ -25,10 +24,11 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(const Duration(seconds: 10), () {
       Navigator.of(context).pushReplacement(MaterialPageRoute(
           builder: (context) =>
-             LoginPage())); //when timer completes it redirects to home
+             const LoginPage())); //when timer completes it redirects to home
     });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +45,7 @@ class _SplashScreenState extends State<SplashScreen> {
                     Colors.orange,
                   ],
                 ).createShader(bounds),
-            child: FaIcon(
+            child: const FaIcon(
               FontAwesomeIcons.instagram,
               size: 100,
             )),
@@ -53,58 +53,54 @@ class _SplashScreenState extends State<SplashScreen> {
       Positioned(
         left: MediaQuery.of(context).size.width * 0.45,
         bottom: MediaQuery.of(context).size.height * 0.13,
-        child: Container(
-          child: Text("from",
-          style: TextStyle(
-            color: Colors.grey,
-            fontSize: 16, 
+          child:const Text(
+            "from",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
             ),
           ),
         ),
-      ),
       Positioned(
-        bottom: MediaQuery.of(context).size.height * 0.0925,
-        left: MediaQuery.of(context).size.width * .4,
-        child: ShaderMask(
+          bottom: MediaQuery.of(context).size.height * 0.0925,
+          left: MediaQuery.of(context).size.width * .4,
+          child: ShaderMask(
             blendMode: BlendMode.srcIn,
             shaderCallback: (Rect bounds) => const LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.deepPurple,
-                    Colors.pink,
-                    Colors.orange,
-                  ],
-                ).createShader(bounds),
-              child: FaIcon(
-                FontAwesomeIcons.meta,
-                size: 25,
-              ),
-            )
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.deepPurple,
+                Colors.pink,
+                Colors.orange,
+              ],
+            ).createShader(bounds),
+            child: const FaIcon(
+              FontAwesomeIcons.meta,
+              size: 25,
             ),
-           Positioned(
-        bottom: MediaQuery.of(context).size.height * 0.0935,
-        left: MediaQuery.of(context).size.width * 0.475,
-        child: ShaderMask(
+          )),
+      Positioned(
+          bottom: MediaQuery.of(context).size.height * 0.0935,
+          left: MediaQuery.of(context).size.width * 0.475,
+          child: ShaderMask(
             blendMode: BlendMode.srcIn,
             shaderCallback: (Rect bounds) => const LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.deepPurple,
-                    Colors.pink,
-                    Colors.orange,
-                  ],
-                ).createShader(bounds),
-              child: Text("Meta",
-         style: TextStyle(
-          fontSize: 20,
-         ),
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.deepPurple,
+                Colors.pink,
+                Colors.orange,
+              ],
+            ).createShader(bounds),
+            child:const Text(
+              "Meta",
+              style: TextStyle(
+                fontSize: 20,
               ),
-            )
             ),
-    ]
-    )
-    );
+          )),
+    ]));
   }
 }
